a126a0d5bada0a51ee643fffca3f17a3a47ea2bb
Anirudh Kaluri <>
2023-07-19 14:27:10 +0530

    shifted to another method to validate better for serialization

--- chess/serializers.py
+++ chess/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
 
-#Another way to implement serializer
-# class HelperSerializer(serializers.Serializer):
-#     Queen=serializers.CharField(required=False)
-#     Bishop=serializers.CharField(required=False)
-#     Rook=serializers.CharField(required=False)
-#     Knight=serializers.CharField(required=False)
-
-# class PositionSerializer(serializers.Serializer):
-#     positions=HelperSerializer()
-#     #positions is a key, its value is a Dictionary
-#     #positions field will be deserialized into a dictionary
+# Another way to implement serializer
+class HelperSerializer(serializers.Serializer):
+    Queen=serializers.CharField(required=False)
+    Bishop=serializers.CharField(required=False)
+    Rook=serializers.CharField(required=False)
+    Knight=serializers.CharField(required=False)
 
 class PositionSerializer(serializers.Serializer):
-    positions=serializers.DictField(child=serializers.CharField())
+    positions=HelperSerializer()
+#     positions is a key, its value is a Dictionary
+#     positions field will be deserialized into a dictionary
+
+# class PositionSerializer(serializers.Serializer):
+#     positions=serializers.DictField(child=serializers.CharField())
