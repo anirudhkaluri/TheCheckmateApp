20d08aaef38e5caa07cba77a3b609b5b9c3e1870
Anirudh Kaluri <>
2023-07-19 23:24:56 +0530

    incorporated a success test case

--- chess/tests.py
+++ chess/tests.py
@@ -1,8 +1,18 @@
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase,APIClient
+from django.http import JsonResponse
 
 # Create your tests here.
 
-
+class PositionTest(TestCase):
+    def setUp(self):
+        self.client=APIClient()
+    
+    def test_valid_positions(self):
+        request_data={"positions": {"Queen": "D5", "Bishop": "H1", "Rook":"A1","Knight":"F3"}}
+        response=self.client.post('/chess/bishop/',request_data,format='json')
+        self.assertEqual(response.status_code,200)
+        expected_response_data={"valid_moves": ["G2"]}
+        self.assertEqual(response.content,JsonResponse(expected_response_data).content)
