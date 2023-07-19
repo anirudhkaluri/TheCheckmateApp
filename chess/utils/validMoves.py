d8d7a8eafe32ba0a03dfcc487718b3d9df631fcc
Anirudh Kaluri <>
2023-07-19 16:28:59 +0530

    implemented teh add_position function

--- chess/utils/validMoves.py
+++ chess/utils/validMoves.py
@@ -17,13 +17,13 @@ def get_valid_moves(board,slug):
     slug_position=get_slug_position(slug,board)
     #pop slug from the board
     board.pop(slug)
-
+    #get a list of all possible moves as if no other piece is on the board except the slug
     possible_moves=get_all_possible_moves(slug,slug_position)
     
 
     return []
 
-
+#returns position of slug in tuple form (row,column)
 def get_slug_position(slug,board):
     cmap={"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8}
     position=board[slug]
@@ -33,17 +33,37 @@ def get_slug_position(slug,board):
 def get_all_possible_moves(slug,starting_position):
     possible_moves=[]
     if slug=="Queen":
-        add_positions(starting_position,possible_moves,diagonal)
-        add_positions(starting_position,possible_moves,horizontal)
-        add_positions(starting_position,possible_moves,vertical)
+        #3 possible directions for queen
+        add_positions(slug,starting_position,possible_moves,[diagonal,horizontal,vertical])
     elif slug=="Bishop":
-        add_positions(starting_position,possible_moves,diagonal)
+        #only diagonal for bishop
+        add_positions(slug,starting_position,possible_moves,[diagonal])
     elif slug=="Rook":
-        add_positions(starting_position,possible_moves,horizontal)
-        add_positions(starting_position,possible_moves,vertical)
+        #horizontal and vertical for rook
+        add_positions(slug,starting_position,possible_moves,[horizontal,vertical])
     elif slug=="Knight":
-        add_positions(starting_position,possible_moves,knight)
+        #knight for knight
+        add_positions(slug,starting_position,possible_moves,[knight])
     return possible_moves
+
+#adds positions based on the direction rules at the beginning of the file
+def add_positions(starting_position,possible_moves,directions):
+    for direction in directions:
+        for item in direction:
+            pos=starting_position
+            pos[0]=pos[0]+item[0]
+            pos[1]=pos[1]+item[1]
+            if slug=="Knight":
+                if check_limit(pos):
+                    possible_moves.append(tuple(pos))
+                    continue
+            else:
+                while check_limit(pos):
+                    possible_moves.append(tuple(pos))
+                    pos[0]=pos[0]+item[0]
+                    pos[1]=pos[1]+item[1]
+                continue
+
 
 
 
