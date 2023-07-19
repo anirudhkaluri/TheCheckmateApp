4585908c9fc1d8c587768a980405ebdf16400a65
Anirudh Kaluri <>
2023-07-19 13:04:55 +0530

    imported dependencies into view

--- chess/views.py
+++ chess/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from rest_framework import APIView
+from rest_framework.response import Response
+from .serializers import PositionSerializer
 # Create your views here.
+
