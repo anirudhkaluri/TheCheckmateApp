d76f3e4a4534796dfda340c4a6253917e05a089f
Anirudh Kaluri <>
2023-07-19 15:58:22 +0530

    implemented get_slug_position method

--- chess/utils/validMoves.py
+++ chess/utils/validMoves.py
@@ -1,3 +1,19 @@
 
-def validMoves():
+def get_valid_moves(board,slug):
+    
+    slug=slug.title()
+
+    #returns a tuple (x,y) x->row y->column
+    slug_position=get_slug_position(slug,board)
+    #pop slug from the board
+    board.pop(slug)
+    
+
     return []
+
+
+def get_slug_position(slug,board):
+    cmap={"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8}
+    position=board[slug]
+    return (int(board[slug][1]),cmap[board[slug][0]])
+
