8a761070ccfe4b68e4783c511ec145ee5f8cca04
Anirudh Kaluri <>
2023-07-19 14:48:11 +0530

    changed the post() definition in position view to receive the slug paramter from the url

--- chess/views.py
+++ chess/views.py
@@ -6,7 +6,8 @@ from .serializers import PositionSerializer
 # Create your views here.
 
 class PositionView(APIView):
-    def post(self,request,format='json'):
+    def post(self,request,slug,format='json'):
+        #serialize the input json data
         serializer=PositionSerializer(data=request.data)
         try:
             serializer.is_valid(raise_exception=True)
@@ -14,6 +15,10 @@ class PositionView(APIView):
             error_details=e.detail
             return Response({'errors':error_details},status=400)
         deserialized_data=serializer.validated_data
+        #board position in an ordered dictionary
+        board=deserialized_data
+        
+
         return Response({'message':deserialized_data})
 
 
