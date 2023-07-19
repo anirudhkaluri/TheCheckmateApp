89e4860eb6889d920c773ba093418d19ac94ceef
Anirudh Kaluri <>
2023-07-19 13:11:55 +0530

    created PositionView in views.py. Code not completely implemented yet. Urls to be configured

--- chess/views.py
+++ chess/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from rest_framework import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from .serializers import PositionSerializer
 # Create your views here.
+
+class PositionView(APIView):
+    def post(self,request,format='json'):
+        serializer=PositionSerializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except ValidationError as e:
+            error_details=e.detail
+            return Response({'errors':error_details},status=400)
+        deserialized_data=serializer.validated_data
+        return Response({'message':deserialized_data})
+
 
