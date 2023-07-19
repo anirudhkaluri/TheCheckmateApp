73365886831e183b13d9e8483caf0739d928bd7a
Anirudh Kaluri <>
2023-07-19 15:39:57 +0530

    imported validMoves from utils, made the function call to get the answer from valid moves. should implement the validMoves function

--- chess/views.py
+++ chess/views.py
@@ -3,22 +3,23 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from .serializers import PositionSerializer
+from .utils import validMoves
 # Create your views here.
 
 class PositionView(APIView):
     def post(self,request,slug,format='json'):
-        #serialize the input json data
+        
+        #deserialize the input json data
         serializer=PositionSerializer(data=request.data)
         try:
             serializer.is_valid(raise_exception=True)
         except ValidationError as e:
             error_details=e.detail
             return Response({'errors':error_details},status=400)
-        deserialized_data=serializer.validated_data
-        #board position in an ordered dictionary
-        board=deserialized_data
-        
 
-        return Response({'message':deserialized_data})
+        #board position in an ordered dictionary
+        board=serializer.validated_data
+        valid_moves= validMoves(board,slug.title())
+        return Response({'message':valid_moves})
 
 
