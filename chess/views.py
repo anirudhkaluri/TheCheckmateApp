67c10f4b50c15445765c860b9b06b5e6ada8b16e
Anirudh Kaluri <>
2023-07-20 14:14:31 +0530

    views.py modified for clear and elegant code

--- chess/views.py
+++ chess/views.py
@@ -7,7 +7,8 @@ from .utils.validMoves import get_valid_moves
 from django.http import JsonResponse
 import logging
 
-
+#define a logger 
+#all logs in project.log in root directory
 logger=logging.getLogger('chess.views')
 
 
@@ -15,24 +16,30 @@ logger=logging.getLogger('chess.views')
 class PositionView(APIView):
     def post(self,request,slug,format='json'):
         
-        #deserialize the input json data
         request.data['slug']=slug
+
+        #Send data to serializer to deserialize and validate
         serializer=PositionSerializer(data=request.data)
         try:
             serializer.is_valid(raise_exception=True)
         except ValidationError as e:
             error_details=e.detail
+            #log both request data and error whenever there is an error/exception
             logger.info(f"Error for the data {request.data}")
             logger.error(error_details)
             return Response({'errors':error_details},status=400)
 
-        #board is a dictionary with position as the only key 
+        #Get data from the serializer.
+        # board is the configuration of the chess board in the form of dictioanry "piece":"piece_position" 
         board=serializer.validated_data['positions']
+        #slug is the piece for which we find the valid moves
         slug=serializer.validated_data['slug']
        
 
-        # board[positions] is an ordered dictionary with locations of teh pieces
+        # use get_valid_moves to get all moves which the slug can take given the board's configuration
         valid_moves= get_valid_moves(board,slug)
+
+        #send response
         response_data={
             'valid_moves':valid_moves
         }
