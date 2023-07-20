2bebbcf022ca8c4e23ace8e4a30642bcec76d7bd
Anirudh Kaluri <>
2023-07-20 14:43:03 +0530

    unit test for given sample test case 1 implemented

--- chess/tests.py
+++ chess/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase,APIClient
 from django.http import JsonResponse
+from .utils.validMoves import get_valid_moves
 
 # Create your tests here.
 
@@ -17,4 +18,13 @@ class PositionTest(TestCase):
         expected_response_data={"valid_moves": ["G2"]}
         self.assertEqual(response.content,JsonResponse(expected_response_data).content) 
     
+
+class ValidMovesTestCase(TestCase):
+    def test_get_valid_moves(self):
+        board={"Queen": "E7", "Bishop": "B7", "Rook":"G5","Knight":"C3"}
+        slug="Knight"
+        output=get_valid_moves(board,slug)
+        output_set=set(output)
+        expected_output_set=set(["B1","D1","A4","A2"])
+        self.assertSetEqual(output_set,expected_output_set)
 
