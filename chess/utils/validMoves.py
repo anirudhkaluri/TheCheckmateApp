9afacb45f10024a58b34bed126fc8d9caf3be90d
Anirudh Kaluri <>
2023-07-20 13:33:28 +0530

    obstruction_exists function fine tuned to re-use get_coordinate_position

--- chess/utils/validMoves.py
+++ chess/utils/validMoves.py
@@ -1,16 +1,16 @@
 import sys
-# MOVE RULES
-#queen travels diagonal, horizontal,vertical
+# DIRECTION RULES
+#Queen travels diagonal, horizontal,vertical
 #Bishop travels diagonal
 #Rook travels horizontal,vertical
-#knight travels Knight
+#Knight travels Knight
 diagonal=[(1,1),(-1,-1),(-1,1),(1,-1)]
 horizontal=[(-1,0),(1,0)]
 vertical=[(0,1),(0,-1)]
 knight=[(2,-1),(2,1),(-2,-1),(-2,1),(1,-2),(-1,-2),(1,2),(-1,2)]
 column_map={"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8}
 row_map={"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8}
-reverse_map={1:"A",2:"B",3:"C",4:"D",5:"E",6:"F",7:"G",8:"H"}
+reverse_map={1:"A",2:"B",3:"C",4:"D",5:"E",6:"F",7:"G",8:"H"} #for converting numerica coordinates to chess terminology
 
 #RETURNS VALID MOVES
 def get_valid_moves(board,slug):
@@ -65,7 +65,7 @@ def add_positions(slug,starting_position,possible_moves,directions,board):
                     possible_moves.append(list(pos))
                     continue
             else:
-                while check_limit(pos) and not obstruction_exists(slug,starting_position,pos,board):
+                while check_limit(pos) and not obstruction_exists(starting_position,pos,board):
                     possible_moves.append(list(pos))
                     pos[0]=pos[0]+item[0]
                     pos[1]=pos[1]+item[1]
@@ -91,14 +91,13 @@ def check_attack_on_positions(possible_moves,board):
 
 #checks whether a piece can travel from one position to another position in one move
 def direct_move_exists(piece,from_position,to_position,board):
-
     same_position=bool(from_position[0]==to_position[0] and from_position[1]==to_position[1])
     if same_position:
         return False
     same_diagonal=bool(abs(from_position[0]-to_position[0])==abs(from_position[1]-to_position[1]))
     same_row= bool(from_position[0]-to_position[0]==0) 
     same_column=bool(from_position[1]-to_position[1]==0)
-    obstruction=bool(obstruction_exists(piece,from_position,to_position,board))
+    obstruction=bool(obstruction_exists(from_position,to_position,board))
     if piece=="Queen" and (same_diagonal or same_row or same_column) and not obstruction:
         return True
     elif piece=="Bishop" and same_diagonal and not obstruction:
@@ -111,25 +110,28 @@ def direct_move_exists(piece,from_position,to_position,board):
                 return True
     return False
 
-#checks whether an obstruction exists between two positions on the board due to another piece
-def obstruction_exists(piece,from_position,to_position,board):
+
+#checks whether an obstruction exists between two positions- position1 and position2
+def obstruction_exists(position1,position2,board):
+    #iterate over the pieces on the board board
     for key,value in board.items():
-        pos=(int(board[key][1]),column_map[board[key][0]])
-        if key==piece or (pos[0]==to_position[0] and pos[1]==to_position[1]) or (pos[0]==from_position[0] and pos[1]==from_position[1]):
+        position3 = get_coordinate_position(board[key])
+        if  (position3[0]==position2[0] and position3[1]==position2[1]) or (position3[0]==position1[0] and position3[1]==position1[1]):
             continue
         else:
-            x1=float(from_position[0])
-            y1=float(from_position[1])
-            x2=float(to_position[0])
-            y2=float(to_position[1])
-            x3=float(pos[0])
-            y3=float(pos[1])
+            x1=float(position1[0])
+            y1=float(position1[1])
+            x2=float(position2[0])
+            y2=float(position2[1])
+            x3=float(position3[0])
+            y3=float(position3[1])
             slope1=(y2-y1)/(x2-x1) if x2!=x1 else sys.maxsize
             slope2=(y3-y2)/(x3-x2) if x3!=x2 else sys.maxsize
-            xmax= from_position[0] if x1>x2 else to_position[0]
-            xmin=from_position[0] if x1<x2 else to_position[0]
-            ymax= from_position[1] if y1>y2 else to_position[1]
-            ymin= from_position[1] if y1<y2 else to_position[1]
-            if slope1==slope2 and (pos[0] in range(xmin,xmax+1)) and (pos[1] in range(ymin,ymax+1)):
+            xmax= position1[0] if x1>x2 else position2[0]
+            xmin=position1[0] if x1<x2 else position2[0]
+            ymax= position1[1] if y1>y2 else position2[1]
+            ymin= position1[1] if y1<y2 else position2[1]
+            #If slopes are equal and if position3's coordinate in between position 1 and position 2's, then obstruction exists
+            if slope1==slope2 and (position3[0] in range(xmin,xmax+1)) and (position3[1] in range(ymin,ymax+1)):
                 return True
     return False
