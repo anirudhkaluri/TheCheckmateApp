2e0a73f5d470652274eae09f7638c68f9e521beb
Anirudh Kaluri <>
2023-07-20 14:20:06 +0530

    added additional validation to make sure no two pieces occupy the same grid position and that there exists only one piece of each type

--- chess/serializers.py
+++ chess/serializers.py
@@ -16,15 +16,25 @@ class PositionSerializer(serializers.Serializer):
         board=attrs.get('positions') #the board configurations
         slug=attrs.get('slug') #the slug received from the url
 
+        set_of_positions=set()
+        set_of_pieces=set()
+
         for piece,position in board.items():
+            
             if piece not in valid_pieces:
-                raise serializers.ValidationError("Invalid chess Pieces given in request data")
+                raise serializers.ValidationError("No such chess pieces. Invalid chess Pieces given in request data")
             if not len(position)==2:
                 raise serializers.ValidationError("Invalid Position.Position on the grid must be of length 2")
             try:
                 [x,y]=get_coordinate_position(position) #uses map. implementation in .utils.validMoves
             except KeyError as e:
                 raise serializers.ValidationError("Invalid position. Position must be of the form [A-H][1-8]")
+            if position in set_of_positions:
+                raise serializers.ValidationError("Two pieces cant be at the same time in the same position")
+            if piece in set_of_pieces:
+                raise serializers.ValidationError("There cant be two pieces of same type.")
+            set_of_pieces.add(piece)
+            set_of_positions.add(position)
     
         if slug not in valid_pieces or slug not in board:
             raise serializers.ValidationError("Invalid slug. Provide a slug present on the board")
