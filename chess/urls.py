b4073a9a2b2cf44602904c352f57be4374d6b64c
Anirudh Kaluri <>
2023-07-21 01:21:54 +0530

    removed the trailing slash

--- chess/urls.py
+++ chess/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import PositionView
 urlpatterns = [
     #represents /chess/slug route. Handled by PositionView to calculate valid_positions 
-    path('<str:slug>/', PositionView.as_view(),name="valid_positions"),
+    path('<str:slug>', PositionView.as_view(),name="valid_positions"),
 ]
