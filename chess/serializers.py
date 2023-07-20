d7ddf9a66242c2e0d1f2d8e2a3b9421be84fc81f
Anirudh Kaluri <>
2023-07-20 13:08:03 +0530

    serializer PositionSerializer modified to validate the slug received from the url parameters

--- chess/serializers.py
+++ chess/serializers.py
@@ -5,8 +5,11 @@ from .utils.validMoves import get_coordinate_position
 
 class PositionSerializer(serializers.Serializer):
     positions=serializers.DictField(child=serializers.CharField())
+    slug=serializers.CharField()
     def validate(self,attrs):
         positions_value=attrs.get('positions')
+        attrs['slug']=attrs.get('slug').title()
+        slug=attrs.get('slug')
         valid_pieces={"Queen","Bishop","Knight","Rook"}
         for key,value in positions_value.items():
             if key not in valid_pieces:
@@ -17,4 +20,6 @@ class PositionSerializer(serializers.Serializer):
                 [x,y]=get_coordinate_position(value)
             except KeyError as e:
                 raise serializers.ValidationError("Invalid position. Position must be of the form [A-H][1-8]")
+        if slug not in valid_pieces or slug not in positions_value:
+            raise serializers.ValidationError("Invalid slug. Provide a slug present on the board")
         return attrs
