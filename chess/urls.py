dab6862caea16a17a88523e8a50b75eff4f9f7ab
Anirudh Kaluri <>
2023-07-21 00:17:12 +0530

    added comments

--- chess/urls.py
+++ chess/urls.py
@@ -2,5 +2,6 @@
 from django.urls import path
 from .views import PositionView
 urlpatterns = [
+    #represents /chess/slug route. Handled by PositionView to calculate valid_positions 
     path('<str:slug>/', PositionView.as_view(),name="valid_positions"),
 ]
