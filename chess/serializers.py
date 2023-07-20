4b0e4489d5149e223266a4ff852a2d5acaf01297
Anirudh Kaluri <>
2023-07-20 12:20:45 +0530

    removed the old implementation of a serializer as two nested serializers. New one takes dictField. The dictionary is validated by overriding the validate function of Serializer class. The validity of the keys and values are checked. Exceptions are raised where ever they are invalid

--- chess/serializers.py
+++ chess/serializers.py
@@ -1,17 +1,20 @@
 from rest_framework import serializers
+from .utils.validMoves import get_coordinate_position
 
-# Another way to implement serializer
-class HelperSerializer(serializers.Serializer):
-    Queen=serializers.CharField(required=False)
-    Bishop=serializers.CharField(required=False)
-    Rook=serializers.CharField(required=False)
-    Knight=serializers.CharField(required=False)
-   
+
 
 class PositionSerializer(serializers.Serializer):
-    positions=HelperSerializer()
-#     positions is a key, its value is a Dictionary
-#     positions field will be deserialized into a dictionary
-
-# class PositionSerializer(serializers.Serializer):
-#     positions=serializers.DictField(child=serializers.CharField())
+    positions=serializers.DictField(child=serializers.CharField())
+    def validate(self,attrs):
+        positions_value=attrs.get('positions')
+        valid_pieces={"Queen","Bishop","Knight","Rook"}
+        for key,value in positions_value.items():
+            if key not in valid_pieces:
+                raise serializers.ValidationError("Invalid chess Pieces given in request data")
+            if not len(value)==2:
+                raise serializers.ValidationError("Invalid Position.Position on the grid must be of length 2")
+            try:
+                [x,y]=get_coordinate_position(value)
+            except KeyError as e:
+                raise serializers.ValidationError("Invalid position. Position must be of the form [A-H][1-8]")
+        return attrs
